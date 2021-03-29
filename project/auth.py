714d9fe0ebe934c6ffa6e1ec49a1822a9ba83f9e
amrl <amirul.ardy@gmail.com>
2021-03-29 16:48:47 +0800

    Add signup input validation

--- project/auth.py
+++ project/auth.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from flask import Blueprint, render_template, redirect, url_for, request, flash, make_response
 from flask_login import login_user, logout_user, login_required
 from werkzeug.security import generate_password_hash, check_password_hash
+import re
 import secrets
 
 
@@ -29,14 +30,14 @@ def login():
             db.session.commit()
 
         if user and not check_password_hash(user.password, password):
-            flash("Error: Incorrect username or password.")
+            flash("Incorrect username or password.")
             user.last_access = current_time
             user.tries = int(user.tries) + 1
             db.session.commit()
             return redirect(url_for("auth.login"))
 
         if not user:
-            flash("Error: Incorrect username or password.")
+            flash("Incorrect username or password.")
             return redirect(url_for("auth.login"))
 
         user.tries = 0
@@ -60,15 +61,23 @@ def signup():
         password2 = request.form.get("password2")
 
         if User.query.filter_by(username=username).first():
-            flash("Error: Username is already registered.")
+            flash("Username is already registered.")
+            return redirect(url_for("auth.signup"))
+
+        if not is_valid_username(username):
+            flash("Username can only contain alphanumeric characters and single hyphens in-between. Max length is 39 characters.")
             return redirect(url_for("auth.signup"))
 
         if User.query.filter_by(email=email).first():
-            flash("Error: Email is already registered.")
+            flash("Email is already registered.")
             return redirect(url_for("auth.signup"))
 
         if password1 != password2:
-            flash("Error: Password confirmation failed.")
+            flash("Password confirmation failed.")
+            return redirect(url_for("auth.signup"))
+
+        if not is_valid_password(password2):
+            flash("Password must be 8 to 64 characters long and contain at least 1 lowercase, 1 uppercase, 1 digit, and 1 special character.")
             return redirect(url_for("auth.signup"))
 
         new_user = User(username=username,
@@ -96,5 +105,26 @@ def sanitize(input):
     pass
 
 
-def validate(input):
-    pass
+'''
+Following GitHub rules:
+Contain only alphanumeric chars or hyphens.
+Cannot have multiple consecutive hyphens.
+Cannot begin or end with a hyphen.
+Max length of 39 chars.
+'''
+def is_valid_username(username):
+    return bool(re.match("^[a-zA-Z\d](?:[a-zA-Z\d]|-(?=[a-zA-Z\d])){0,38}$",
+                         username))
+
+
+'''
+Following NIST recommendations of minimum 8 characters,
+and max to (at least) 64 characters.
+At least 1 digit.
+At least 1 lowercase letter.
+At least 1 uppercase letter.
+At least 1 special character.
+'''
+def is_valid_password(password):
+    return bool(re.match("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[*.!@$%^&(){}[\]:;<>,.?\/~_+-=|]).{8,64}$",
+                         password))
